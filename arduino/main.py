d6d002a25de23ace6248240f4976df610ef7d8f7
Carson Miner <miner188@umn.edu>
2020-01-27 17:58:07 -0600

    Created robot makeup data structure.

--- arduino/main.py
+++ arduino/main.py
@@ -1,41 +1,53 @@
 import pyfirmata
 import time
 
-board = pyfirmata.Arduino('/dev/cu.usbmodem14401')
+#board = pyfirmata.Arduino('/dev/cu.usbmodem14401')
 
-arm = {
+robot = {
     "base": {
         "pin": 3,
-        "initLocation": 0},
-    "forearm": {
+        "location": 0},
+    "shoulder": {
         "pin": 4,
-        "initLocation": 0},
+        "location": 0},
+    "elbow": {
+        "pin": 5,
+        "location": 0},
+    "wrist": {
+        "pin": 5,
+        "location": 0},
     "claw": {
         "pin": 5,
-        "initLocation": 0},
+        "location": 0},
     }
 
-}
-
-while True:
-    board.digital[13].write(1)
-    time.sleep(1)
-    board.digital[13].write(0)
-    time.sleep(1)
+# while True:
+#     board.digital[13].write(1)
+#     time.sleep(1)
+#     board.digital[13].write(0)
+#     time.sleep(1)
 
 
 # noinspection PyUnreachableCode
-class ArmControl:
+class Robot:
     def __init__(self, arm):
         print("Control initiated")
         self.arm = arm
 
     def moveToPoint(self, x, y, z):
         print("Moving to point (" + x + "," + y + "," + z + ").")
-        # TODO: Accomplish using pyFirmata
+        # TODO: Accomplish using BreezyArm
 
-    def moveServo(self, servo, angle):
+    def moveComponent(self, servo, angle):
         print("Moving " + servo + "to" + angle + ".")
+        # Move servo
+        robot[servo]["location"] = angle
 
     def __str__(self):
-        return "TODO"
+        message = ""
+        for component in robot:
+            message += component + ": "
+            for attribute in component:
+                message += attribute + " "
+            message += "\n"
+        return message
